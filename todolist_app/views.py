a728670aa8499f6ee2f3fcc82547baa883452695
ArpitVishnoi <>
2020-09-01 18:09:29 +0530

    Supper User

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -5,7 +5,7 @@ from todolist_app.models import Tasklist
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
-
+ 
 @login_required
 def todolist(request):
     if request.method == "POST":
